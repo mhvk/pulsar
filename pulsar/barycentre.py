6e336139eca91672281ad9802346a1d2c1fd71d2
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2013-07-30 14:14:55 -0400

    barycentre: Treat Time objects in full precision in calls to jplephem

--- pulsar/barycentre.py
+++ pulsar/barycentre.py
@@ -1,11 +1,12 @@
+"""Update to ephemeris from jplephem package to include 'earth' as an option,
+and to allow one to pass Time objects instead of straight MJDs"""
 import jplephem.ephem
 
 
-# update to ephemeris from jplephem package to include 'earth' as an option
 class JPLEphemeris(jplephem.ephem.Ephemeris):
     """JPLEphemeris, but including 'earth'"""
 
-    def position(self, name, tdb):
+    def position(self, name, tdb, tdb2=0.):
         """Compute the position of `name` at time `tdb`.
 
         Run the `names()` method on this ephemeris to learn the values
@@ -15,12 +16,15 @@ class JPLEphemeris(jplephem.ephem.Ephemeris):
         each of the three return values ``(x, y, z)`` will be an array.
 
         """
-        if name == 'earth':
-            return self._interpolate_earth(tdb, differentiate=False)
-        else:
-            return self._interpolate(name, tdb, differentiate=False)
+        if hasattr(tdb, 'tdb'):  # Time object
+            tdb, tdb2 = tdb.tdb.jd1, tdb.tdb.jd2
 
-    def compute(self, name, tdb):
+        if name == 'earth':
+            return self._interpolate_earth(tdb, tdb2, differentiate=False)
+        else:
+            return self._interpolate(name, tdb, tdb2, differentiate=False)
+
+    def compute(self, name, tdb, tdb2=0.):
         """Compute the position and velocity of `name` at time `tdb`.
 
         Run the `names()` method on this ephemeris to learn the values
@@ -30,15 +34,18 @@ class JPLEphemeris(jplephem.ephem.Ephemeris):
         each of the six return values ``(x, y, z, dx, dy, dz)`` will be
         an array.
         """
-        if name == 'earth':
-            return self._interpolate_earth(tdb, differentiate=True)
-        else:
-            return self._interpolate(name, tdb, differentiate=True)
+        if hasattr(tdb, 'tdb'):  # Time object
+            tdb, tdb2 = tdb.tdb.jd1, tdb.tdb.jd2
 
-    def _interpolate_earth(self, tdb, differentiate):
-        earthmoon_ssb = self._interpolate('earthmoon', tdb,
+        if name == 'earth':
+            return self._interpolate_earth(tdb, tdb2, differentiate=True)
+        else:
+            return self._interpolate(name, tdb, tdb2, differentiate=True)
+
+    def _interpolate_earth(self, tdb, tdb2, differentiate):
+        earthmoon_ssb = self._interpolate('earthmoon', tdb, tdb2,
                                           differentiate=differentiate)
-        moon_earth = self._interpolate('moon', tdb,
+        moon_earth = self._interpolate('moon', tdb, tdb2,
                                        differentiate=differentiate)
         # earth relative to Moon-Earth barycentre
         # earth_share=1/(1+EMRAT), EMRAT=Earth/Moon mass ratio
